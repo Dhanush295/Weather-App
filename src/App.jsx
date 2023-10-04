e7515792f1fc578d0b89bd9862051f43e38f5dfc
Dhanush <dhanushg295@gmail.com>
2023-10-04 14:47:57 -0700

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,11 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-      backgroundImage: `url("../components/images/weather.jpg")`,
-      backgroundSize: "cover", 
-      backgroundRepeat: "no-repeat",
-    }}>
       <Router>
         <Routes>
           <Route path="/" element={<Weather />} />
         </Routes>
       </Router>
-    </div>
   );
 }
 
